88eb9db4ee7bbcacc96c4888a7fa71e3805f8fd8
Val <dgm97@cornell.edu>
2016-01-06 18:20:57 -0500

    Modified project cover images

--- includes/projects.php
+++ includes/projects.php
@@ -1,21 +1,49 @@
-<div class="large-box" id="lbx1">
-	image
-</div>
-<div class="med-box" id="mbx1">
-	image
-</div>
-<div class="med-box" id="mbx2">
-	image
-</div>
-<div class="small-box" id="sbx1">
-	image
-</div>
-<div class="small-box" id="sbx2">
-	image
-</div>
-<div class="small-box" id="sbx3">
-	image
-</div>
-<div class="small-box" id="sbx4">
-	image
+
+
+
+
+<div class="ui link cards">
+  <div class="card">
+    <a class="image" href="#">
+  		<img src="/images/avatar/large/steve.jpg">
+  	</a>
+  	<div class="content">
+  		<a class="header" href="#">Steve Jobes</a>
+  		<div class="meta">
+  			<a>Last Seen 2 days ago</a>
+  		</div>
+  	</div>
+  </div>
+  <div class="card">
+    <div class="image">
+      <img src="/images/avatar2/large/molly.png">
+    </div>
+    <div class="content">
+      <div class="header">Molly</div>
+      <div class="meta">
+        <span class="date">Coworker</span>
+      </div>
+    </div>
+    <div class="extra content">
+      <span class="right floated">
+        Joined in 2011
+      </span>
+    </div>
+  </div>
+  <div class="card">
+    <div class="image">
+      <img src="/images/avatar2/large/elyse.png">
+    </div>
+    <div class="content">
+      <div class="header">Elyse</div>
+      <div class="meta">
+        <a>Coworker</a>
+      </div>
+    </div>
+    <div class="extra content">
+      <span class="right floated">
+        December 2014
+      </span>
+    </div>
+  </div>
 </div>
