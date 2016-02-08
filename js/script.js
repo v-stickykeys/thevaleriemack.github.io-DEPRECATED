05d4e4394476de9cc1ee63dfa8c20b05ae252701
Val <dgm97@cornell.edu>
2016-02-08 18:16:38 -0500

    contact links

--- js/script.js
+++ js/script.js
@@ -87,6 +87,9 @@ $(document).ready(function(){
 if (document.documentElement.clientWidth < 700) {
   document.getElementById("leftSide").className = "one wide";
   document.getElementById("mnCntnt").className = "fifteen wide column";
+} else {
+  document.getElementById("leftSide").className = "four wide column";
+  document.getElementById("mnCntnt").className = "twelve wide column";
 }
 
 // loop
