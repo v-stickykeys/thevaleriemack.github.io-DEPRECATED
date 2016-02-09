84b793ae8b70d6c42c61d584f37e6e8398aee08a
Val <dgm97@cornell.edu>
2016-02-09 01:30:24 -0500

    remove js breakpoint

--- js/script.js
+++ js/script.js
@@ -84,14 +84,6 @@ $(document).ready(function(){
 });
 
 
-if (document.documentElement.clientWidth < 700) {
-  document.getElementById("leftSide").className = "one wide";
-  document.getElementById("mnCntnt").className = "fifteen wide column";
-} else {
-  document.getElementById("leftSide").className = "four wide column";
-  document.getElementById("mnCntnt").className = "twelve wide column";
-}
-
 // loop
 // $(document).scroll(function(){
 //       if (document.documentElement.clientHeight + $(window).scrollTop() >= $(document).height()) {
