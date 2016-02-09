b9831d9f430d592f1e4e1575f08afcf97399e3c8
Val <dgm97@cornell.edu>
2016-02-09 01:36:33 -0500

    update column width on mobile

--- js/script.js
+++ js/script.js
@@ -85,8 +85,8 @@ $(document).ready(function(){
 
 
 if (document.documentElement.clientWidth < 700) {
-  document.getElementById("leftSide").className = "one wide";
-  document.getElementById("mnCntnt").className = "fifteen wide column";
+  document.getElementById("leftSide").className = "two wide";
+  document.getElementById("mnCntnt").className = "fourteen wide column";
 } else {
   document.getElementById("leftSide").className = "four wide column";
   document.getElementById("mnCntnt").className = "twelve wide column";
